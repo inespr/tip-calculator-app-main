2d45d1f40943f69a1fd1ea3e6327c2d874e88fd5
inespr <iperrom@g.educaand.es>
2023-01-11 13:00:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,23 @@ var bill = document.getElementById("bill").value;
 var numP = document.getElementById("numP").value;
 
 function selectTip(){
-    var tip = document.getElementsByName().value;
+    var tip = document.getElementsById("tip").value;
+    switch (tip) {
+        case tip:
+            
+            break;
+    
+        default:
+            break;
+    }
 }
 function selectPeople(){
+   
+}
+
+function calculo() {
     if(numP == 0 ){
         var error = document.getElementById("error").value;
         error.style.visibility = "visible";
-    }
+    }s
 }
