47b9e1bec5fb08f13a13a1224bf1df014dffa68d
inespr <iperrom@g.educaand.es>
2023-01-16 12:04:48 +0100

    Commit-8

--- main.js
+++ main.js
@@ -2,49 +2,40 @@ let bill;
 let numP;
 let tip;
 
-function selectTip(tip){
-    var boton = document.getElementById(tip);
-    boton.addEventListener("click", ()=> {
-    boton.style.color = "hsl(183, 100%, 15%)";
-    boton.style.backgroundColor = "hsl(172, 67%, 45%)";
-    });
-     console.log(tip);
-     return tip;
+function selectTip(val){
+    tip = val;
+    console.log(tip);
 }
 
 function selectTipCust() {
     tip = document.getElementById("tipCustom").value;
     console.log(tip);
-    return tip;
 }
 
 function selectPeople(){
     var error = document.getElementById("error");
     numP = document.getElementById("numP").value;
-    if(numP == 0 ){
+    if(numP == 0 || Number.isInteger(numP) == false){
         error.style.visibility = "visible";
     }
     else{
         error.style.visibility = null;
         console.log(numP);
-        return numP;
     }
 }
 
 function selectBill() {
     bill = document.getElementById("bill").value;
     console.log(bill);
-    return bill;
 }
 
 function calculo() {
-    while(tip!=null && numP!=null && bill!=null){
+    if(tip && bill){
         var tipAmount=((bill*tip)/100)/numP;
         console.log(tipAmount);
-        document.getElementById("resultTipAmount").innerHTML = tipAmount;
+        document.getElementById("resultTipAmount").innerHTML = "$" + tipAmount.toFixed("2");
         var totalP = (tipAmount + (bill/numP));
-        console.log(totalP)
-        document.getElementById("resultTotal").innerHTML = totalP;
-    }
+        console.log(totalP);
+        document.getElementById("resultTotal").innerHTML = "$" + totalP.toFixed("2");
+   }
 }
-
